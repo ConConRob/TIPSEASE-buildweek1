ebbf9ea0eeaee1b4bdd6f496f842f55335365b78
conconrob <connortprobertson@gmail.com>
2019-02-06 13:07:07 +0000

    ADDED show and remove funvtionality to login modal

--- components/login-modal/login-modal.js
+++ components/login-modal/login-modal.js
@@ -4,17 +4,19 @@ class Login {
     this.button = loginButton;
     this.loginMenu = document.querySelector('.login-modal');
     loginButton.addEventListener('click', () => this.showLogin());
+    this.closeButton = this.loginMenu.querySelector('.close-modal');
+    this.closeButton.addEventListener('click', () => this.hideLogin());
   }
 
   showLogin() {
-    console.log(this.loginMenu);
     this.loginMenu.classList.remove('hide');
+  }
+
+  hideLogin() {
+    this.loginMenu.classList.add('hide');
   }
 }
 
 // target all instances of login buttons
 const loginButtons = document.querySelectorAll('.login');
 loginButtons.forEach(loginButton => new Login(loginButton));
-console.log(loginButtons)
-const test = document.querySelector('.login-modal');
-console.log(test);
