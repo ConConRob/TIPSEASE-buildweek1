1e684a942a99f846e37aa1bb3290ea450d99b21a
conconrob <connortprobertson@gmail.com>
2019-02-07 14:00:33 +0000

    made the mobile nav close when a link is clicked

--- components/mobile-nav/mobile-nav.js
+++ components/mobile-nav/mobile-nav.js
@@ -2,10 +2,19 @@
 const openButton = document.querySelector(".open-nav");
 const mobileNav = document.querySelector(".mobile-nav");
 const closeButton = document.querySelector(".close-nav");
-openButton.addEventListener("click", () => {
+// select mobile nav links
+const mobileNavLinks = document.querySelectorAll(".mobile-nav .links a");
+console.log(mobileNavLinks);
+openButton.addEventListener("click", openMobileNav);
+closeButton.addEventListener("click", closeMobileNav);
+// if link is clicked in mobile nav, close the mobile nav
+mobileNavLinks.forEach(link => link.addEventListener('click', closeMobileNav));
+
+function openMobileNav() {
   // mobileNav.style.display = "flex";
   TweenMax.to(mobileNav, 0.4, { opacity: 1, display: 'flex' });
-});
-closeButton.addEventListener("click", () => {
+}
+
+function closeMobileNav() {
   TweenMax.to(mobileNav, 0.4, { opacity: 0, display: 'none' });
-});
+}
