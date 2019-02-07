6f0d3765c4d58890f1a7d796ab72890d6821f32a
conconrob <connortprobertson@gmail.com>
2019-02-07 20:33:35 +0000

    put nav js in iff

--- components/mobile-nav/mobile-nav.js
+++ components/mobile-nav/mobile-nav.js
@@ -1,19 +1,21 @@
-// select nav
-const openButton = document.querySelector(".open-nav");
-const mobileNav = document.querySelector(".mobile-nav");
-const closeButton = document.querySelector(".close-nav");
-// select mobile nav links
-const mobileNavLinks = document.querySelectorAll(".mobile-nav .links a");
-openButton.addEventListener("click", openMobileNav);
-closeButton.addEventListener("click", closeMobileNav);
-// if link is clicked in mobile nav, close the mobile nav
-mobileNavLinks.forEach(link => link.addEventListener('click', closeMobileNav));
+(function () {
+  // select nav
+  const openButton = document.querySelector(".open-nav");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const closeButton = document.querySelector(".close-nav");
+  // select mobile nav links
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav .links a");
+  openButton.addEventListener("click", openMobileNav);
+  closeButton.addEventListener("click", closeMobileNav);
+  // if link is clicked in mobile nav, close the mobile nav
+  mobileNavLinks.forEach(link => link.addEventListener('click', closeMobileNav));
 
-function openMobileNav() {
-  // mobileNav.style.display = "flex";
-  TweenMax.to(mobileNav, 0.4, { opacity: 1, display: 'flex' });
-}
+  function openMobileNav() {
+    // mobileNav.style.display = "flex";
+    TweenMax.to(mobileNav, 0.4, { opacity: 1, display: 'flex' });
+  }
 
-function closeMobileNav() {
-  TweenMax.to(mobileNav, 0.4, { opacity: 0, display: 'none' });
-}
+  function closeMobileNav() {
+    TweenMax.to(mobileNav, 0.4, { opacity: 0, display: 'none' });
+  }
+})();
